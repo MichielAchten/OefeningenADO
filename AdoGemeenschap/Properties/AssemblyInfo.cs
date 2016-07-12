e0cc09bcd9d93603aaa50f925e68ad6c836880f5
MichielAchten <michiel.achten@gmail.com>
2016-07-12 13:43:15 +0200

    ddd

--- AdoGemeenschap/Properties/AssemblyInfo.cs
+++ AdoGemeenschap/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c98221ac-f7b1-49d5-882d-c7008bf7b515")]
+[assembly: Guid("9280f4fb-6954-428c-a6ac-35a30d2d5890")]
 
 // Version information for an assembly consists of the following four values:
 //
