ea4ae401341b5d8f33b5991390ee5463720ae977
MichielAchten <michiel.achten@gmail.com>
2016-07-12 13:34:49 +0200

    consoleApp

---
+++ ConsoleApplicationADO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hzallo");
+        }
+    }
+}
