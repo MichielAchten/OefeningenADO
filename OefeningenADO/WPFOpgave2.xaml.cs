bc78f25a3b97a62b177c59e47b53080fc0a45c4d
MichielAchten <michiel.achten@gmail.com>
2016-07-12 11:43:29 +0200

    ConnectiesTesten2

---
+++ OefeningenADO/WPFOpgave2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using AdoGemeenschap;
+
+namespace OefeningenADO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class WPFOpgave2 : Window
+    {
+        public WPFOpgave2()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //var manager = new TuincentrumDbManager();
+                //using (var conTuincentrum = manager.GetConnection())
+                using (var conTuincentrum = new TuincentrumDbManager().GetConnection())
+                {
+                    conTuincentrum.Open();
+                    labelStatus.Content = "Tuincentrum geopend";
+                }
+            }
+            catch (Exception ex)
+            {
+                labelStatus.Content = ex.Message;
+            }
+        }
+    }
+}
