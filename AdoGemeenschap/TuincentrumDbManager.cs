da30623e409aa8e69568d79ed90f9f898975ae96
MichielAchten <michiel.achten@gmail.com>
2016-07-13 14:19:40 +0200

    OpgaveCommandClass1

--- AdoGemeenschap/TuincentrumDbManager.cs
+++ AdoGemeenschap/TuincentrumDbManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using System.Data.Common;
+using System.Data;
 
 namespace AdoGemeenschap
 {
@@ -18,6 +19,42 @@ namespace AdoGemeenschap
             var conTuincentrum = factory.CreateConnection();
             conTuincentrum.ConnectionString = conTuincentrumSetting.ConnectionString;
             return conTuincentrum; 
+        }
+
+        public Boolean LeverancierToevoegen(String naam, String adres, String postcode, String plaats)
+        {
+            var dbManager = new TuincentrumDbManager();
+            using (var conTuincentrum = dbManager.GetConnection())
+            {
+                using (var comLeverancierToevoegen = conTuincentrum.CreateCommand())
+                {
+                    comLeverancierToevoegen.CommandType = CommandType.Text;
+                    comLeverancierToevoegen.CommandText = "LeverancierToevoegen";
+
+                    DbParameter parNaam = comLeverancierToevoegen.CreateParameter();
+                    parNaam.ParameterName = "@naam";
+                    parNaam.Value = naam;
+                    comLeverancierToevoegen.Parameters.Add(parNaam);
+
+                    DbParameter parAdres = comLeverancierToevoegen.CreateParameter();
+                    parAdres.ParameterName = "@adres";
+                    parAdres.Value = adres;
+                    comLeverancierToevoegen.Parameters.Add(parAdres);
+
+                    DbParameter parPostcode = comLeverancierToevoegen.CreateParameter();
+                    parPostcode.ParameterName = "@postcode";
+                    parPostcode.Value = postcode;
+                    comLeverancierToevoegen.Parameters.Add(parPostcode);
+
+                    DbParameter parPlaats = comLeverancierToevoegen.CreateParameter();
+                    parPlaats.ParameterName = "@plaats";
+                    parPlaats.Value = plaats;
+                    comLeverancierToevoegen.Parameters.Add(parPlaats);
+
+                    conTuincentrum.Open();
+                    return comLeverancierToevoegen.ExecuteNonQuery() != 0;
+                }
+            }
         }
     }
 }
